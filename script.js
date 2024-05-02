84983a0d7f676578e143f871ab4064e9bae2d193
IgorCavalcantiMoura <icavalcantimoura@gmail.com>
2024-05-02 18:13:05 -0300

    link page

---
+++ script.js
@@ -0,0 +1,15 @@
+
+const lightMode = document.querySelector("#modo_light");
+const elementSwitch = document.querySelector("#switch");
+const img = document.querySelector("#profile img");
+
+elementSwitch.addEventListener("click", () => {
+  lightMode.classList.toggle("light");
+
+  if (lightMode.classList.contains("light")) {
+    img.setAttribute("src", "./assets/assets/igor_light_mode2.png");
+  } else {
+    img.setAttribute("src", "./assets/assets/igor_dark_mode.png");
+  }
+});
+
